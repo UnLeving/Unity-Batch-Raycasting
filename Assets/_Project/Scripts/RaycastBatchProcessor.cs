ca03f878c914f02a4b23d9374ff472ca1bc44cca
ALeving <fgileving@gmail.com>
2025-09-04 14:05:33 +0300

    init after fork; fix broken tween package;

--- Assets/_Project/Scripts/RaycastBatchProcessor.cs
+++ Assets/_Project/Scripts/RaycastBatchProcessor.cs
@@ -4,7 +4,8 @@ using Unity.Collections;
 using Unity.Jobs;
 using UnityUtils;
 
-public class RaycastBatchProcessor : Singleton<RaycastBatchProcessor> {
+public class RaycastBatchProcessor : Singleton<RaycastBatchProcessor>
+{
     [SerializeField] int maxRaycastsPerJob = 10000;
 
     NativeArray<RaycastCommand> rayCommands;
@@ -18,21 +19,26 @@ public class RaycastBatchProcessor : Singleton<RaycastBatchProcessor> {
         bool hitBackfaces,
         bool hitTriggers,
         bool hitMultiFace,
-        Action<RaycastHit[]> callback) {
+        Action<RaycastHit[]> callback)
+    {
         const float maxDistance = 1f;
         int rayCount = Mathf.Min(origins.Length, maxRaycastsPerJob);
 
-        QueryTriggerInteraction queryTriggerInteraction = hitTriggers ? QueryTriggerInteraction.Collide : QueryTriggerInteraction.Ignore;
+        QueryTriggerInteraction queryTriggerInteraction =
+            hitTriggers ? QueryTriggerInteraction.Collide : QueryTriggerInteraction.Ignore;
 
-        using (rayCommands = new NativeArray<RaycastCommand>(rayCount, Allocator.TempJob)) {
-            QueryParameters parameters = new QueryParameters {
+        using (rayCommands = new NativeArray<RaycastCommand>(rayCount, Allocator.TempJob))
+        {
+            QueryParameters parameters = new QueryParameters
+            {
                 layerMask = layerMask,
                 hitBackfaces = hitBackfaces,
                 hitTriggers = queryTriggerInteraction,
                 hitMultipleFaces = hitMultiFace
             };
 
-            for (int i = 0; i < rayCount; i++) {
+            for (int i = 0; i < rayCount; i++)
+            {
                 rayCommands[i] = new RaycastCommand(origins[i], directions[i], parameters, maxDistance);
             }
 
@@ -40,19 +46,23 @@ public class RaycastBatchProcessor : Singleton<RaycastBatchProcessor> {
         }
     }
 
-    void ExecuteRaycasts(NativeArray<RaycastCommand> raycastCommands, Action<RaycastHit[]> callback) {
+    void ExecuteRaycasts(NativeArray<RaycastCommand> raycastCommands, Action<RaycastHit[]> callback)
+    {
         int maxHitsPerRaycast = 1;
         int totalHitsNeeded = raycastCommands.Length * maxHitsPerRaycast;
 
-        using (hitResults = new NativeArray<RaycastHit>(totalHitsNeeded, Allocator.TempJob)) {
-            foreach (RaycastCommand t in raycastCommands) {
+        using (hitResults = new NativeArray<RaycastHit>(totalHitsNeeded, Allocator.TempJob))
+        {
+            foreach (RaycastCommand t in raycastCommands)
+            {
                 Debug.DrawLine(t.from, t.from + t.direction * 1f, Color.red, 0.5f);
             }
 
             JobHandle raycastJobHandle = RaycastCommand.ScheduleBatch(raycastCommands, hitResults, maxHitsPerRaycast);
             raycastJobHandle.Complete();
 
-            if (hitResults.Length > 0) {
+            if (hitResults.Length > 0)
+            {
                 RaycastHit[] results = hitResults.ToArray();
 
                 // for (int i = 0; i < results.Length; i++) {
